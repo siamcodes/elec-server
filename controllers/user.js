99415954ce6c394bcda8d4aa376b54bde0b4b814
siamcodes <siamcodes@gmail.com>
2021-08-16 00:14:24 +0700

    initial

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 const User = require("../models/user");
 const Product = require("../models/product");
 const Cart = require("../models/cart");
+const Coupon = require("../models/coupon");
 
 exports.userCart = async (req, res) => {
   // console.log(req.body); // {cart: []}
@@ -27,7 +28,7 @@ exports.userCart = async (req, res) => {
     // get price for creating total
     //let { price } = await Product.findById(cart[i]._id).select("price").exec();
     //object.price = price;
-    
+
     let productFromDb = await Product.findById(cart[i]._id)
       .select("price")
       .exec();
@@ -81,5 +82,41 @@ exports.saveAddress = async (req, res) => {
   ).exec();
 
   res.json({ ok: true });
+};
+
+
+exports.applyCouponToUserCart = async (req, res) => {
+  const { coupon } = req.body;
+  console.log("COUPON", coupon);
+
+  const validCoupon = await Coupon.findOne({ name: coupon }).exec();
+  if (validCoupon === null) {
+    return res.json({
+      err: "Invalid coupon",
+    });
+  }
+  console.log("VALID COUPON", validCoupon);
+
+  const user = await User.findOne({ email: req.user.email }).exec();
+
+  let { products, cartTotal } = await Cart.findOne({ orderedBy: user._id })
+  .populate("products.product", "_id title price")
+  .exec();
+
+  console.log("cartTotal", cartTotal, "discount%", validCoupon.discount);
+
+  // calculate the total after discount
+  let totalAfterDiscount = (
+    cartTotal -
+    (cartTotal * validCoupon.discount) / 100
+  ).toFixed(2); // 99.99
+
+  Cart.findOneAndUpdate(
+    { orderedBy: user._id },
+    { totalAfterDiscount },
+    { new: true }
+  );
+
+  res.json(totalAfterDiscount);
 };
 
