b30c0261fff3cbba5222e0c929a4b2a8886387fe
siamcodes <siamcodes@gmail.com>
2022-01-12 21:23:19 +0700

    initial

--- routes/user.js
+++ routes/user.js
@@ -5,18 +5,37 @@ const router = express.Router();
 const { authCheck } = require("../middlewares/auth");
 
 // controllers
-const { userCart, getUserCart, emptyCart, saveAddress, applyCouponToUserCart, createOrder, orders } = require("../controllers/user");
+const { 
+    userCart, 
+    getUserCart, 
+    emptyCart, 
+    saveAddress, 
+    applyCouponToUserCart, 
+    createOrder, 
+    orders,
+    addToWishlist,
+    wishlist,
+    removeFromWishlist,
+    createCashOrder,
+} = require("../controllers/user");
 
 router.post("/user/cart", authCheck, userCart); // save cart
 router.get("/user/cart", authCheck, getUserCart); // get cart
 router.delete("/user/cart", authCheck, emptyCart); // empty cart
 router.post("/user/address", authCheck, saveAddress);
 
-router.post("/user/order", authCheck, createOrder);
+router.post("/user/order", authCheck, createOrder); //stripe
+router.post("/user/cash-order", authCheck, createCashOrder); // cod
 router.get("/user/orders", authCheck, orders);
 
 // coupon
 router.post("/user/cart/coupon", authCheck, applyCouponToUserCart);
+
+// wishlist
+router.post("/user/wishlist", authCheck, addToWishlist);
+router.get("/user/wishlist", authCheck, wishlist);
+router.put("/user/wishlist/:productId", authCheck, removeFromWishlist);
+
 
 // router.get("/user", (req, res) => {
 //   res.json({
